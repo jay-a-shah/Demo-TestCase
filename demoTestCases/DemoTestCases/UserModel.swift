3f82e3dee5894341fed73b8093fc299aaf90152a
Jay Shah <jay.a.shah@simformsolutions.com>
2022-08-31 10:56:15 +0530

    Unit Test Cases

---
+++ demoTestCases/DemoTestCases/UserModel.swift
@@ -0,0 +1,14 @@
+//
+//  UserModel.swift
+//  DemoTestCases
+//
+//  Created by Jay Shah on 30/08/22.
+//
+
+import Foundation
+
+struct UserModel {
+    var firstName : String
+    var email : String
+    var password : String
+}
